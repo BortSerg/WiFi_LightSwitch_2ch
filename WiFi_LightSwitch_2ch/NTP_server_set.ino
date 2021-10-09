bb7454fa4f51998bf64739840701a520f7e4f34b
Bort Sergey <bort.sergei@gmail.com>
2021-10-10 00:09:16 +0300

    add android apk for smartphone control lightserver

--- WiFi_LightSwitch_2ch/NTP_server_set.ino
+++ WiFi_LightSwitch_2ch/NTP_server_set.ino
@@ -2,7 +2,7 @@ void SNTP_server_set(){
   sntp_stop();
   sntp_setservername(0,"ntp.time.in.ua");
   sntp_setservername(1,"pool.ntp.org");
-  sntp_setservername(2,"ntp2.time.in.ua");
+  sntp_setservername(2,"192.168.3.1");
   sntp_set_timezone(Config.GMT);
   sntp_init();
   delay (1000);
